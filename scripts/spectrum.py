fbd89cd997fdf058eb79ed6d35b673a238b89554
Andrea Cocut <andrea.cocut@gmail.com>
2024-05-07 15:28:28 +0200

    Additional options for a user to choose from.

--- scripts/spectrum.py
+++ scripts/spectrum.py
@@ -15,19 +15,55 @@ def main():
         "--input-file",
         type=str,
         metavar=("input_file"),
+        default="qm.out",
         help="Name of the files that contain oscillator strengths and excitation energies.")
     parser.add_argument(
         "-b",
         "--bandwidth",
-        nargs='?',
+        default=0.1,
         type=float,
         metavar=("bandwidth"),
         help="Give the value of the bandwith of Gaussian functions.")
+    parser.add_argument(
+        "-eu",
+        "--energy-unit",
+        choices=["eV", "nm"],
+        type=str,
+        metavar=("energy_unit"),
+        default="eV",
+        help="Choose the units to express the energy on the x-axis of the spectrum. Available options are eV and nm.")
+    parser.add_argument(
+        "-n",
+        "--normalization",
+        action='store_true',
+        help="Create the spectrum with intensities relative to the maximum peak which is set to have the value of 1.")
+    parser.add_argument(
+        "-sv",
+        "--start-value",
+        type=float,
+        metavar=("start_value"),
+        default=0,
+        help="Set the start value of energy on the x-axis.")
+    parser.add_argument(
+        "-ev",
+        "--end-value",
+        type=float,
+        metavar=("end_value"),
+        default=10,
+        help="Set the end value of energy on the x-axis.")
+    parser.add_argument(
+        "-s",
+        "--step",
+        type=float,
+        metavar=("step"),
+        default=0.01,
+        help="Set the value of the step (distance between two points) of the energy axis.")
+
     args = parser.parse_args()
 
     input_file = args.input_file
     bandwidth = args.bandwidth
-    
+
     all_os = []
     all_excited = []
 
@@ -45,15 +81,24 @@ def main():
         intensity = 0
         for i in range(len(all_os)):
             for j in range(len(all_os[0])):
-               #intensity += 1.22 * 10**(-44)* 1/bandwidth * 1/100 * float(all_os[i][j]) * float(all_excited[i][j]) * np.exp(-1/2 *((en - float(all_excited[i][j]))/bandwidth) **2)
-                intensity +=  1/bandwidth * float(all_os[i][j]) * float(all_excited[i][j]) * np.exp(-1/2 *((en - float(all_excited[i][j]))/bandwidth) **2)
+                if args.energy_unit == "nm":
+                    intensity +=  1/bandwidth * float(all_os[i][j]) * 1239.8/(float(all_excited[i][j])) * np.exp(-1/2 *((1239.8/en - 1239.8/(float(all_excited[i][j])))/bandwidth) **2)
+                if args.emergy_unit == "eV":
+                    intensity +=  1/bandwidth * float(all_os[i][j]) * float(all_excited[i][j]) * np.exp(-1/2 *((en - float(all_excited[i][j]))/bandwidth) **2)
         return float(intensity)
 
     f = np.vectorize(nea_spect)
 
-    en = np.arange(3, 8, 0.1)
+    en = np.arange(args.start_value, args.end_value, args.step)
 
     plt.plot(en, f(en))
+
+    if args.normalization:
+        g= f(en)/f(en).max()
+
+
+
+    #en = np.arange(3, 8, 0.1)
 
     plt.show()
 
